cbcfbb75e34000a0b7cb55b6a917edf3cc4dd5ab
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-05-24 15:16:16 +0530

    js-code-backup

---
+++ calculator-project/script.js
@@ -0,0 +1,29 @@
+alert("Provide two numbers to get their addition, subtraction, multiplication and division results");
+
+const number1 = Number(prompt("Enter 1st number"));
+
+const number2 = Number(prompt("Enter 2nd number"));
+
+const additionResult  = number1 + number2;
+
+alert("The result of addition is " +  additionResult);
+
+
+const subtractionResult  = number1 - number2;
+
+alert("The result of subtraction is " +  subtractionResult);
+
+
+const multiplicationResult  = number1 * number2;
+
+alert("The result of multiplication is " +  multiplicationResult);
+
+
+const divisionResult  = number1 / number2;
+
+alert("The result of division is " +  divisionResult);
+
+
+
+
+
