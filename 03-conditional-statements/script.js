41fac499ac26ff8cd0d7949fdc30099b3813b0a2
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-06-10 19:41:29 +0530

    small change in conditional statements script file and added swap two numbers code

--- 03-conditional-statements/script.js
+++ 03-conditional-statements/script.js
@@ -4,7 +4,7 @@ if (marks >= 90) {
 alert("A Grade");
 } else if (marks >= 60) {
 alert("B Grade");
-}else if (marks >= 40) {
+} else if (marks >= 40) {
 alert("C Grade");
 } else {
 alert("We appreciate your efforts.Try again next time, you will succeed In sha Allah");
---
+++ 04-swap-two-numbers/script.js
@@ -0,0 +1,17 @@
+alert("Enter two numbers to swap them");
+
+let number1 = Number(prompt("Enter 1st number"));
+
+let number2 = Number(prompt("Enter 1st number"));
+
+alert("The data stored in variables number1 and number2 before swapping: " + number1 + " " + number2);
+ 
+let temporary;
+
+temporary = number1;
+
+number1 = number2;
+
+number2 = temporary;
+
+alert("The data stored in variables number1 and number2 after swapping: " + number1 + " " + number2);
