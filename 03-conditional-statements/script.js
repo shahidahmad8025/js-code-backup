ed5a007ae0293cfbf9673d9ef3cb67a71083527e
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-06-10 19:15:03 +0530

    Folders name rename and conditional statements code added

---
+++ 03-conditional-statements/script.js
@@ -0,0 +1,11 @@
+const marks = Number(prompt("Enter your marks"));
+
+if (marks >= 90) {
+alert("A Grade");
+} else if (marks >= 60) {
+alert("B Grade");
+}else if (marks >= 40) {
+alert("C Grade");
+} else {
+alert("We appreciate your efforts.Try again next time, you will succeed In sha Allah");
+}
