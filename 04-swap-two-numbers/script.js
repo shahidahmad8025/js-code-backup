d4c21d413fa6d9005f461b13cebe121f7d0f5c64
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-06-14 17:55:53 +0530

    changes in script file

--- 04-swap-two-numbers/script.js
+++ 04-swap-two-numbers/script.js
@@ -1,15 +1,13 @@
-alert("Enter two numbers to swap them");
-
-let number1 = Number(prompt("Enter 1st number"));
-
-let number2 = Number(prompt("Enter 2nd number"));
+let number1 = 3;
+let number2 = 5;
 
 alert("The data stored in variables number1 and number2 before swapping: " + number1 + " " + number2);
- 
+
 let temporary = number1;
 
 number1 = number2;
 
-number2 = temporary;
+number2  = temporary;
 
 alert("The data stored in variables number1 and number2 after swapping: " + number1 + " " + number2);
+
