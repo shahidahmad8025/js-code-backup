220bc468a5393378b5ff9b77d8d7d64048ae1c2b
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-06-14 17:19:03 +0530

    small mod in script file

--- 04-swap-two-numbers/script.js
+++ 04-swap-two-numbers/script.js
@@ -2,7 +2,7 @@ alert("Enter two numbers to swap them");
 
 let number1 = Number(prompt("Enter 1st number"));
 
-let number2 = Number(prompt("Enter 1st number"));
+let number2 = Number(prompt("Enter 2nd number"));
 
 alert("The data stored in variables number1 and number2 before swapping: " + number1 + " " + number2);
  
