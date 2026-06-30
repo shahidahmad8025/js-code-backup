b11c95fe8b28ae4382db44b577af2c728e67797d
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-06-30 11:22:15 +0530

    added rectangle area, perimeter program

---
+++ 05-rectangle-perimeter/script.js
@@ -0,0 +1,7 @@
+const length = 10;
+const width = 5;
+
+const perimeter = 2 * (length + width);
+
+
+alert("Perimeter of the rectangle:", perimeter);
---
+++ 06-reactangle-area/script.js
@@ -0,0 +1,7 @@
+const length = 10;
+const width = 5;
+
+const area = length * width;
+
+
+alert("Area of the rectangle:", area);
