2bd1474fad4e045d86a7662a36b672fc348bf1b3
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-06-30 10:08:45 +0530

    mod in template file and added greatest no. program code

---
+++ 05-largest-number/script.js
@@ -0,0 +1,17 @@
+const num1 = 1;
+
+const num2 = 2;
+
+const num3 = 3;
+
+let largestNumber = num1;
+
+if (num2 > largestNumber) {
+largestNumber = num2;
+}
+
+if(num3 > largestNumber) {
+largestNumber = num3;
+}
+
+alert("The greatest number is " + largestNumber);
