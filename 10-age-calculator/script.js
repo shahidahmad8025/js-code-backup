26cacd3553b319a443f8c58668c184ef244b4157
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-08-07 18:32:05 +0530

    small mod in age cal script file

--- 10-age-calculator/script.js
+++ 10-age-calculator/script.js
@@ -1,6 +1,6 @@
 const birthYear = Number(prompt("Enter your birth year"));
 
-const currentYear = 2026
+const currentYear = Number(prompt("Enter the current year"));
 
 const age = currentYear - birthYear;
 
