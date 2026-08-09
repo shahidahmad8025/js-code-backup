0a73d954febc0575bef6470d252156a7ca58e83e
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-08-09 20:49:17 +0530

    added monthly salary calculator program

---
+++ 11-monthly-salary/script.js
@@ -0,0 +1,7 @@
+const dailyWage = Number(prompt("Enter the daily wage:"));
+
+const workingDays = Number(prompt("Enter the number of working days:"));
+
+const monthlySalary = dailyWage * workingDays;
+
+alert("The monthly salary is " + monthlySalary + " rupees");
