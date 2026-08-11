cd5170f3949968080042705db16d2c3bf9e3e429
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-08-11 19:16:53 +0530

    modifed total-marks file and add algo for it

--- 09-totalmarks-percentage/script.js
+++ 09-totalmarks-percentage/script.js
@@ -2,9 +2,11 @@ const subject1 = Number(prompt("Enter the marks obtained in subject1"));
 const subject2 = Number(prompt("Enter the marks obtained in subject2"));
 const subject3 = Number(prompt("Enter the marks obtained in subject3"));
 
-const totalMarks = subject1 + subject2 + subject3;
+const marksObtained = subject1 + subject2 + subject3;
 
-const percentage = (totalMarks / 300) * 100;
+const totalMarks = 300;
+
+const percentage = (marksObtained / totalMarks) * 100;
 
 alert("Total Marks = " + totalMarks);
 
