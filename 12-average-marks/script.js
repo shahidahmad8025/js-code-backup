135eaeaa19a68567a67dd65a2668f02d7c4bf46e
Shahid Ahmad <shahidahmad8025@gmail.com>
2026-08-17 11:55:24 +0530

    added algo for monthly salary and average marks, and average marks program

---
+++ 12-average-marks/script.js
@@ -0,0 +1,7 @@
+const marks1 = Number(prompt("Enter marks of the first student out of 100:"));
+const marks2 = Number(prompt("Enter marks of the second student out of 100:"));
+const marks3 = Number(prompt("Enter marks of the third student out of 100:"));
+
+const averageMarks = (marks1 + marks2 + marks3) / 3;
+
+alert("The average marks are " + average);
